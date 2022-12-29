2ec0eb5899fa8450c0f919a2f11fa7948d2d8d8c
yansans <66671259+yansans@users.noreply.github.com>
2022-12-29 23:55:54 +0700

    refactor: iterator instead of loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
+use colored::Colorize;
 use std::env;
 use std::error::Error;
 use std::fs;
-use colored::Colorize;
 
 pub struct Config {
     query: String,
@@ -10,13 +10,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough argument");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(s) => s,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(s) => s,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -47,50 +52,30 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let colored_result = colorize_query(&config.query, result);
 
-    for line in colored_result {
-        println!("{line}")
-    }
+    colored_result.iter().for_each(|line| println!("{}", line));
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line)
-        }
-    }
-
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line)
-        }
-    }
-
-    result
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query.to_lowercase().as_str()))
+        .collect()
 }
 
-pub fn colorize_query<'a> (query: &str, result: Vec<&str>) -> Vec<String>{
-
-    let mut colored_result = Vec::new();
-
-    for line in result {
-        let colored_line = line.replace(query, &query.yellow().to_string());
-        colored_result.push(colored_line);
-    }
-
-    colored_result
-
+pub fn colorize_query(query: &str, result: Vec<&str>) -> Vec<String> {
+    result
+        .iter()
+        .map(|line| line.replace(query, &query.yellow()))
+        .collect()
 }
 
 #[cfg(test)]
